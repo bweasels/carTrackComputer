8669231480630192fade81c75238a974845ee0bb
Ben Wesley <bweasels@CarComp.carcomp.wrx>
2020-01-13 05:00:51 -0500

    got a dial to pop up on my screen!

---
+++ testRun.py
@@ -0,0 +1,46 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.floatlayout import FloatLayout
+from kivy.config import Config
+from oldScripts.dials import CircularProgressBar
+from kivy.clock import Clock
+
+Config.set('graphics', 'width', '1024')
+Config.set('graphics', 'height', '600')
+
+
+class TimingWindow(Screen):
+    pass
+
+class OptionsWindow(Screen):
+    pass
+
+#wm = ScreenManager()
+#wm.add_widget(TimingWindow(name="timingWindow"))
+#wm.add_widget(OptionsWindow(name="OptionsWindow"))
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("testrun.kv")
+
+class testRunApp(App):
+    def animate(self, dt):
+        bar = self.root.get_screen('timingWindow').ids.test
+        if bar.value < bar.max:
+            bar.value += 1
+        else:
+            bar.value = bar.min
+#        for bar in self.root.children:
+#            if bar.value < bar.max:
+#            bar.value += 1
+#else:
+#    bar.value = bar.min
+    
+    def build(self):
+        Clock.schedule_interval(self.animate, 0.05)
+        return kv
+
+if __name__ == "__main__":
+    testRunApp().run()
